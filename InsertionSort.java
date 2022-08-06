1887f896112afa062a9308cdaba20d5d25c46231
Mykyta Tymko <tymkomykyta2108@gmail.com>
2022-08-06 17:50:37 +0200

    06.08.2022
    Fix insertion sort

--- InsertionSort.java
+++ InsertionSort.java
@@ -7,7 +7,7 @@ public class InsertionSort {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
-                j = j - 1;
+                j--;
             }
         }
     }
